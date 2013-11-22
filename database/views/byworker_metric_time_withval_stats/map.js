07b89475887da869ead44ab126b4de18ce321b04
Adam Cox <gadamc@cloudant.com>
2013-11-22 11:01:47 -0800

    changing map function to handle new schema

--- database/views/byworker_metric_time_withval_stats/map.js
+++ database/views/byworker_metric_time_withval_stats/map.js
@@ -1,7 +1,7 @@
 function(doc){
-  if(doc.worker_id && doc.metrics && doc.type === 'interval' && doc.timestamp){
+  if(doc.instance_id && doc.metrics && doc.type === 'interval' && doc.timestamp){
     for(var i in doc.metrics){
-      emit([doc.worker_id, doc.metrics[i]['name']].concat(doc.timestamp), doc.metrics[i]['val']);
+      emit([doc.instance_id, doc.metrics[i]['name']].concat(doc.timestamp), doc.metrics[i]['val']);
     }
   }
 }
