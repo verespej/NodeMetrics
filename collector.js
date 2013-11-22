dba2ee643c3a0bbf8992f3316b119b64c30f8245
Adam Cox <gadamc@cloudant.com>
2013-11-21 19:24:03 -0800

    using ip address for worker id

--- collector.js
+++ collector.js
@@ -5,8 +5,23 @@ var nano = require('nano')('https://nodemetrics:msopentechhackathon@nodemetrics.
    , db_name = "workerdata"
    , db      = nano.use(db_name);
 
-var myid = 'worker1';
 
+//get ipaddress... assume first non-internal ipv4 address...?
+var os = require('os')
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family == 'IPv4' && !address.internal) {
+            addresses.push(address.address)
+        }
+    }
+}
+
+var myadd = addresses[0];
+var mypid = process.pid;
+var myid = myadd+'p'+mypid;
 
 setInterval(function() {
 	var cpuUsage = cpuInfo.usage();
@@ -19,6 +34,8 @@ setInterval(function() {
   doc = {
     _id: myid + ':' + now.toISOString(),
     worker_id: myid,
+    ipaddress: addresses[0],
+    pid: mypid,
     timestamp: [
       now.getUTCFullYear(), 
       now.getUTCMonth(), 
